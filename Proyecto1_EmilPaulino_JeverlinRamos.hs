74d307ed3468c34d928b07e3cf1302a2a7066cd9
Jeverlin <jmrc0005@ce.pucmm.edu.do>
2026-03-24 15:13:21 -0400

    Cambios en el menu, se agregaron comentarios explicativos

--- Proyecto1_EmilPaulino_JeverlinRamos.hs
+++ Proyecto1_EmilPaulino_JeverlinRamos.hs
@@ -11,28 +11,28 @@
 
 -- Evaluacion de errores
 -- Tipo definido para poder representar los errores posibles durante la evaluacion de una expresion
-data EvalError = DivisionPorCero | OperacionInvalida | VariableNoDefinida String
+-- Se utiliza Either para representar los exitos (con Right) y los fallos (con Left)
+data EvalError = DivisionPorCero | VariableNoDefinida String
 instance Show EvalError where
     show DivisionPorCero = "Error. Division por cero"
     show (VariableNoDefinida var) = "Error. La variable " ++ var ++ " no esta definida"
-    show OperacionInvalida = "Error. Operacion invalida"
+
 
 -- Tipos algebraicos
 -- Definicion del tipo algebraico propio, solo es posible suma, resta, multiplicacion y division
 
--- Se define este tipo algebraico con las operaciones basicas aritmeticas de suma, resta, multiplicacion y division. Esto con el proposito de realizar las operaciones
--- y poder utilizar pattern matching
+-- Se define este tipo algebraico con las operaciones basicas aritmeticas de suma, resta, multiplicacion y division.
+-- Esto con el proposito de realizar las operaciones y poder utilizar pattern matching
 
-data TipoAlgebraico = Suma | Resta | Multiplicacion | Division 
+data Operacion = Suma | Resta | Multiplicacion | Division 
 
--- La funcion aplicarTipo recibe un operador (que es TipoAlgebraico), y dos numeros a, y devuelve otro numero si todo es ejecutado correctamente
+-- La funcion aplicarTipo recibe un operador (que es Operacion), y dos numeros a, y devuelve otro numero si todo es ejecutado correctamente
 -- a: es un tipo de dato general, que representa que puede ser cualquier tipo de numero
 -- Se utiliza Fractional a, Eq a para permitir operar con distintos tipos de numeros y que la division y la comparacion con cero sea correcta
 
-aplicarTipo :: (Fractional a, Eq a) => TipoAlgebraico -> a -> a -> Either EvalError a
+aplicarTipo :: (Fractional a, Eq a) => Operacion -> a -> a -> Either EvalError a
 
 -- Definicion de las funciones con pattern matching
-
 -- Suma
 aplicarTipo Suma x y = Right (x + y)
 
@@ -49,28 +49,10 @@ aplicarTipo Division _ 0 = Left DivisionPorCero
 aplicarTipo Division x y = Right (x / y)
 
 -- Definir expresion recursiva
-
 -- Tipo de dato que representa una expresion aritmetica, un valor o una operacion aplicada a dos expresiones
 -- La expresion aritmetica a evaluar puede ser un valor sencillo o una operacion que se aplique a dos expresiones,
--- lo que permite construir expresiones complejas a partir de expresiones pequeñas
-data Expr a = Valor a | Var String |Aplicar TipoAlgebraico (Expr a) (Expr a)
-
--- Funcion que evalua de forma recursiva una expresion y maneja errores usando Either
-evalua :: (Fractional a, Eq a) => Expr a -> Either EvalError a
-
--- Evalua de forma recursiva una expresion y retorna Either EvalError a
--- Si ambas subexpresiones evaluan correctamente (Right), se aplica el operador
--- Si alguna de estas expresiones falla, se propaga el error con Left
--- El caso Var retorna OperacionInvalida porque evalua no tiene acceso al entorno
-
-evalua (Valor n) = Right n
-evalua (Var _) = Left OperacionInvalida
-evalua (Aplicar op exp1 exp2) =
-  combinar (evalua exp1) (evalua exp2)
-  where
-    combinar (Right valor1) (Right valor2) = aplicarTipo op valor1 valor2
-    combinar (Left err) _ = Left err
-    combinar _ (Left err) = Left err
+-- lo que permite construir expresiones simples o anidadas
+data Expr a = Valor a | Var String |Aplicar Operacion (Expr a) (Expr a)
 
 -- Recolectar variables
 -- Recorre el arbol de forma recursiva y recolecta los nombres de todas las variables usadas
@@ -106,8 +88,9 @@ instance Show a => Show (Expr a) where
     show (Var nombre) = nombre
     show (Aplicar op e1 e2) = "(" ++ show e1 ++ " " ++ showOp op ++ " " ++ show e2 ++ ")"
 
--- Funcion auxiliar que muestra el simbolo del operador
-showOp :: TipoAlgebraico -> String
+-- Convierte una operacion a su simbolo matematico correspondiente
+-- Funcion auxiliar pura que muestra el simbolo del operador usando instance Show Expr
+showOp :: Operacion -> String
 showOp Suma = "+"
 showOp Resta = "-"
 showOp Multiplicacion = "*"
@@ -138,6 +121,15 @@ evaluar env (Aplicar op e1 e2) =
 evaluarTodas :: Env -> [Expr Double] -> [Either EvalError Double]
 evaluarTodas env = foldr (\ expr acc -> evaluar env expr : acc) []
 
+-- Suma todos los resultados validos de una lista de Either EvalError Double
+-- Ignora los errores (Left) y va acumulando solo los valores exitosos (Right)
+-- Usa foldr para recorrer la lista declarativamente
+sumaResultadosValidos :: [Either EvalError Double] -> Double
+sumaResultadosValidos = foldr sumar 0
+  where
+    sumar (Right x) acc = x + acc
+    sumar (Left _)  acc = acc
+
 -- Profundidad del arbol de expresiones
 -- profundidad es una funcion pura y recursiva que calcula la altura del arbol de expresiones
 -- Caso base Valor y Var: son hojas del arbol, profundidad 0
@@ -165,6 +157,13 @@ procesarResultado = agregarEtiqueta . formatear
 -- Interaccion con el usuario
 -- Separa la logica y el IO
 
+-- Para garantizar la unicidad de las variables
+-- Inserta una variable en el entorno garantizando que no haya duplicados
+-- Si la variable ya existe, la reemplaza con el nuevo valor
+insertarVariable :: String -> Double -> Env -> Env
+insertarVariable nombre valor env =
+    (nombre, valor) : filter (\(n, _) -> n /= nombre) env
+
 -- Pide al usuario que defina una variable y la agrega al entorno
 definirVariable :: Env -> IO Env
 definirVariable env = do
@@ -175,7 +174,7 @@ definirVariable env = do
     case (reads valStr :: [(Double, String)]) of
         [(val, "")] -> do
             putStrLn $ "Variable " ++ nombre ++ " = " ++ show val ++ " definida"
-            return ((nombre, val) : env)
+            return (insertarVariable nombre val env)
         _ -> do
             putStrLn "Valor invalido, ingrese un numero valido"
             definirVariable env
@@ -228,6 +227,10 @@ pedirOperandoDerecho env = do
             putStrLn "  Opcion invalida, ingrese 1, 2 o 3"
             pedirOperandoDerecho env
 
+-- Construye la expresion aritmetica de forma interactiva y recursiva
+-- El usuario escoge primero el operando izquierdo, luego la operacion, luego el operando derecho
+-- Si el usuario elige anidar, se llama a si misma de forma recursiva para construir la subexpresion
+-- No evalua, unicamente construye el arbol de tipo Expr
 construirExpr :: Env -> IO (Expr Double)
 construirExpr env = do
     putStrLn ""
@@ -240,7 +243,7 @@ construirExpr env = do
     return (Aplicar op izq der)
 
 -- Pide al usuario que elija una operacion aritmetica
-pedirOperacion :: IO TipoAlgebraico
+pedirOperacion :: IO Operacion
 pedirOperacion = do
     putStrLn "  Seleccione la operacion:"
     putStrLn "  1. Suma"
@@ -257,45 +260,208 @@ pedirOperacion = do
             putStrLn "  Opcion invalida, ingrese 1, 2, 3 o 4"
             pedirOperacion
 
+-- Muestra el historial de expresiones evaluadas con el entorno actual
+-- Usa evaluarTodas para evaluar nuevamente todas las expresiones con foldr
+-- Muestra cada resultado usando procesarResultado y la suma de resultados validos
+mostrarHistorial :: Env -> [Expr Double] -> IO ()
+mostrarHistorial env historial = do
+    putStrLn ""
+    putStrLn "----- HISTORIAL -----"
+
+    let resultados = evaluarTodas env historial
+    let suma = sumaResultadosValidos resultados
+
+    mapM_ (putStrLn . procesarResultado) resultados
+    putStrLn $ "Suma de resultados validos: " ++ show suma
+
 -- Menu principal recursivo
 -- El usuario puede definir variables, evaluar expresiones o salir
-menuPrincipal :: Env -> IO ()
-menuPrincipal env = do
+menuPrincipal :: Env -> [Expr Double] -> IO ()
+menuPrincipal env historial = do
     putStrLn ""
     putStrLn "----- MENU PRINCIPAL -----"
     putStrLn $ "Variables definidas: " ++ show (map fst env)
     putStrLn "1. Definir variable"
     putStrLn "2. Construir y evaluar expresion"
-    putStrLn "3. Salir"
+    putStrLn "3. Ver historial"
+    putStrLn "4. Salir"
     opcion <- getLine
+
     case opcion of
         "1" -> do
             nuevoEnv <- definirVariable env
-            menuPrincipal nuevoEnv
+            menuPrincipal nuevoEnv historial
+
         "2" -> do
             expr <- construirExpr env
             putStrLn ""
-            putStrLn $ "Expresion construida: "  ++ show expr
-            putStrLn $ "Variables usadas: "      ++ show (recolectarVariables expr)
+            putStrLn $ "Expresion construida: " ++ show expr
+            putStrLn $ "Variables usadas: " ++ show (recolectarVariables expr)
+            putStrLn $ "Profundidad del arbol: " ++ show (profundidad expr)
             putStrLn $ procesarResultado (evaluar env expr)
-            menuPrincipal env
-        "3" -> putStrLn "SALIENDO"
-        _   -> do
-            putStrLn "Opcion invalida "
-            menuPrincipal env
+            menuPrincipal env (historial ++ [expr])
 
--- Punto de entrada del programa
+        "3" -> do
+            mostrarHistorial env historial
+            menuPrincipal env historial
+
+        "4" -> putStrLn "SALIENDO"
+
+        _ -> do
+            putStrLn "Opcion invalida"
+            menuPrincipal env historial
+
+-- MAIN, interaccion con el usuario
 main :: IO ()
 main = do
     putStrLn "---- Interprete de lenguaje funcional ----"
     putStrLn "---- (Evaluador de expresiones aritmeticas) ----"
-    menuPrincipal []
+    menuPrincipal [] []
 
 -- Conclusion
--- En este proyecto número 2, se amplió el proyecto 1 sobre evaluacion de expresiones 
--- Se pudo implementar el manejo de errores utilizando Either, que permite indicar a que se debio el fallo
--- Ademas, se agrego el soporte de variables, haciendo uso de un entorno para evaluar recursivamente, la funcion es buscarVar
--- En ese mismo sentido, se aplico composicion funcional (con .) en la funcion procesarResultado, y fold de forma explicita en la funcion evaluarTodas
--- Por otro lado, se definieron instancias de Show para las funciones EvalError y Expr, separando el IO de la logica
--- El menu principal tambien mantiene separacion entre la logica y el IO, siendo recursivo
+-- En este proyecto se extendio el evaluador de expresiones del proyecto anterior.
+-- Se incorporo manejo de errores mediante Either, lo que permite representar fallos
+-- explicitamente y de forma segura. Ademas, se agrego soporte para variables por medio
+-- de un entorno de evaluacion y una busqueda recursiva con buscarVar.
+-- Tambien se utilizaron otras herramientas propias del paradigma funcional, como recursion
+-- estructural sobre Expr, composicion de funciones en procesarResultado y foldr en
+-- evaluarTodas y sumaResultadosValidos. Del mismo modo, se mantuvo una separacion clara
+-- entre la logica pura y la interaccion con el usuario mediante IO.
 
+{- 
+
+EJEMPLOS DE COMO NAVEGAR EL MENU
+
+===== EJEMPLO 1 =====
+
+----- MENU PRINCIPAL -----
+Variables definidas: []
+1. Definir variable
+2. Construir y evaluar expresion
+3. Ver historial
+4. Salir
+1
+Nombre de la variable:
+x
+Valor de la variable:
+2
+Variable x = 2.0 definida
+
+----- MENU PRINCIPAL -----
+Variables definidas: ["x"]
+1. Definir variable
+2. Construir y evaluar expresion
+3. Ver historial
+4. Salir
+2
+
+Primer valor:
+  1. Numero
+  2. Variable (disponibles: ["x"])
+1
+  Ingrese el numero:
+3
+Operacion:
+  Seleccione la operacion:
+  1. Suma
+  2. Resta
+  3. Multiplicacion
+  4. Division
+1
+Segundo valor:
+  1. Numero
+  2. Variable (disponibles: ["x"])
+  3. Otra operacion (anidar)
+2
+  Ingrese el nombre de la variable:
+x
+
+Expresion construida: (3.0 + x)
+Variables usadas: ["x"]
+Profundidad del arbol: 1
+[Evaluacion] Resultado: 5.0
+
+===== EJEMPLO 2 =====
+---- Interprete de lenguaje funcional ----
+---- (Evaluador de expresiones aritmeticas) ----
+
+----- MENU PRINCIPAL -----
+Variables definidas: []
+1. Definir variable
+2. Construir y evaluar expresion
+3. Ver historial
+4. Salir
+1
+Nombre de la variable:
+x
+Valor de la variable:
+3
+Variable x = 3.0 definida
+
+----- MENU PRINCIPAL -----
+Variables definidas: ["x"]
+1. Definir variable
+2. Construir y evaluar expresion
+3. Ver historial
+4. Salir
+1
+Nombre de la variable:
+y
+Valor de la variable:
+2
+Variable y = 2.0 definida
+
+----- MENU PRINCIPAL -----
+Variables definidas: ["y","x"]
+1. Definir variable
+2. Construir y evaluar expresion
+3. Ver historial
+4. Salir
+2
+
+Primer valor:
+  1. Numero
+  2. Variable (disponibles: ["y","x"])
+1
+  Ingrese el numero:
+5
+Operacion:
+  Seleccione la operacion:
+  1. Suma
+  2. Resta
+  3. Multiplicacion
+  4. Division
+1
+Segundo valor:
+  1. Numero
+  2. Variable (disponibles: ["y","x"])
+  3. Otra operacion (anidar)
+3
+
+Primer valor:
+  1. Numero
+  2. Variable (disponibles: ["y","x"])
+2
+  Ingrese el nombre de la variable:
+x
+Operacion:
+  Seleccione la operacion:
+  1. Suma
+  2. Resta
+  3. Multiplicacion
+  4. Division
+3
+Segundo valor:
+  1. Numero
+  2. Variable (disponibles: ["y","x"])
+  3. Otra operacion (anidar)
+2
+  Ingrese el nombre de la variable:
+y
+
+Expresion construida: (5.0 + (x * y))
+Variables usadas: ["x","y"]
+Profundidad del arbol: 2
+[Evaluacion] Resultado: 11.0
+
+-}
